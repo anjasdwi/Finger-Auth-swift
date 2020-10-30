8e50f9caecf5e4f68dcb8ce996e00160385543e6
Anjas Dwi <anjasdwi@bareksa.com>
2020-10-30 14:17:36 +0700

    Initial Commit

---
+++ finger_authentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finger_authentication
+//
+//  Created by Anjas Dwi on 30/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
